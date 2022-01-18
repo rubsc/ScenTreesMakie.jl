dabed43ed5438bdbc867de1dc0c56906093cb5eb
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-18 17:02:22 +0100

    updated tee_plot_makie.jl

--- src/tree_plot_makie.jl
+++ src/tree_plot_makie.jl
@@ -9,11 +9,15 @@ Args:
 
 Using the Makie version no Python has to be installed.
 """
-function tree_plot(trr::Tree)
+function tree_plot(trr::Tree,fig = nothing, tit = nothing), simple= false
 
     stg = stage(trr)
     
-    f = Figure(backgroundcolor = :gray80, resolution = (1000, 700))
+    if fig === nothing
+        f = Figure(backgroundcolor = :gray80, resolution = (1000, 700))
+    else
+        f = fig;
+    end
 
     ga = f[1, 1] = GridLayout(); gb = f[1, 2] = GridLayout();
 
@@ -21,18 +25,28 @@ function tree_plot(trr::Tree)
     axright = Axis(ga[1, 2]);
     #linkyaxes!(axmain, axright);
 
-    
-    for i = 1 : length(trr.parent)
-        if stg[i] > 0
-            tmp = DataFrame(x=[stg[i],stg[i]+1], y= [trr.state[trr.parent[i]],trr.state[i]])
-            lines!(axmain,tmp.x,tmp.y)
+    if (simple == true)
+        for i = 1 : length(trr.parent)
+            if stg[i] > 0
+                if (trr.state[trr.parent[i]] != 0 && trr.probability[trr.parent[i]] > 0)
+                    if trr.state[i] != 0 && trr.probability[i] >0
+                        tmp = DataFrame(x=[stg[i],stg[i]+1], y= [trr.state[trr.parent[i]],trr.state[i]])
+                        lines!(axmain,tmp.x,tmp.y)
+                    end
+                end
+            end
         end
+
+    else # simple= false, DEFAULT
+        for i = 1 : length(trr.parent)
+            if stg[i] > 0
+                tmp = DataFrame(x=[stg[i],stg[i]+1], y= [trr.state[trr.parent[i]],trr.state[i]])
+                lines!(axmain,tmp.x,tmp.y)
+            end
+        end
+
     end
     
-    f
-
-    # calling p provides the first plot
-
     (Yi,_,probYi) = leaves(trr)
     Yi = [trr.state[i] for i in Yi]
     
