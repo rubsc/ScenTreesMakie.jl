257b0290194df6ecf01d38e8cd81e8b6e164dcc7
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-18 15:30:04 +0100

    updated export

--- src/ScenTrees.jl
+++ src/ScenTrees.jl
@@ -9,7 +9,9 @@ include("checkTree.jl")
 #include("trees_plot.jl")
 include("trees_plot_makie.jl")
 export tree_approximation!,lattice_approximation,Tree,Lattice,stage,height,leaves,nodes,root,
-        part_tree,build_probabilities!,tree_plot,plot_hd,plot_lattice,gaussian_path1D,gaussian_path2D,
+        part_tree,build_probabilities!,
+        tree_plot,#plot_hd,plot_lattice,
+        gaussian_path1D,gaussian_path2D,
         running_maximum1D,running_maximum2D,path,bushiness_nesdistance,kernel_scenarios, checkTree, tree_plot2, tree_plot3,
         tree_path,
 end
