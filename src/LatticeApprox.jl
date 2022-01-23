46df890f251b59b757010be61c53a0b8671a63db
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-23 21:18:06 +0100

    added example for lattices

--- src/LatticeApprox.jl
+++ src/LatticeApprox.jl
@@ -112,3 +112,23 @@ function plot_lattice(lt::Lattice,fig = nothing, title = nothing)
     return(f)
 
 end
+
+
+
+function Lattice(identifier::Int64)
+    self= new()
+    if identifier == 302
+        self.name = "Lattice 1x2x2"
+        self.state = [[0.0],[-0.856;0.933],[-1.27;1.307]]
+        self.probability = [[1.0],[0.52;0.48],[0.75 0.25;0.26 0.74]]
+    elseif identifier == 303
+        self.name = "Lattice 1x1x4"
+        self.state = [[0.0],[0.02],[-1.27;1.307;2.2;1.4]]
+        self.probability = [[1.0],[1.0],[0.25 0.25 0.25 0.25]]
+    elseif identifier == 304
+        self.name = "Tree 1x2x3x4"
+        self.state = [[0.0],[-0.856;0.933],[-1.27;1.307;2.0],[-2.5;-0.5;2.5;0.5]]
+        self.probability = [[1.0],[0.52;0.48],[0.35 0.35 0.3; 0.1 0.5 0.4],[0.25 0.25 0.25 0.25;0.25 0.25 0.25 0.25;0.25 0.25 0.25 0.25]]
+    end
+    return self
+end
