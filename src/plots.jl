3d0aa8f13d81ff951fab6fc9a0eafc6c7c0e07f1
rubsc <58817466+rubsc@users.noreply.github.com>
2022-03-01 08:06:55 +0100

    Update plots.jl

--- src/plots.jl
+++ src/plots.jl
@@ -48,6 +48,67 @@ end
 
 
 
+
+
+"""
+	tree_plot!(trr::Tree,fig=1)
+
+Returns the plot of the input tree annotated with density of
+probabilities of reaching the leaf nodes in the tree.
+Args:
+- trr - A scenario tree.
+- fig - Specifies the size of the image you want to be returned, default = 1.
+
+Using the Plots version no Python has to be installed, the gr() backend works best at the moment (Fast and easy to use).
+"""
+function tree_plot!(trr::Tree{A,B,C,D},fig = nothing, title = nothing, simple= false, label=nothing) where {A,B,C,D}
+
+    stg = stage(trr)
+    tmpX = []; tmpY = [];
+    if (simple == true)
+        for i = 1 : length(trr.parent)
+            if stg[i] > 0
+                if (trr.state[trr.parent[i]] != 0 && trr.probability[trr.parent[i]] > 0)
+                    if trr.state[i] != 0 && trr.probability[i] >0
+			x=[stg[i];stg[i]+1]; y= [trr.state[trr.parent[i]];trr.state[i]];
+                    	tmpX = append!(tmpX,x,NaN)
+                    	tmpY = append!(tmpY,y,NaN)
+                    end
+                end
+            end
+        end
+
+    else # simple= false, DEFAULT
+        for i = 1 : length(trr.parent)
+            if stg[i] > 0
+                x=[stg[i];stg[i]+1]; y= [trr.state[trr.parent[i]];trr.state[i]];
+                tmpX = append!(tmpX,x,NaN)
+                tmpY = append!(tmpY,y,NaN)
+            end
+        end
+
+    end
+    
+    if label=== nothing
+        plot!(tmpX,tmpY,legend=:topleft);
+    else
+        plot!(tmpX,tmpY,legend=:topleft, label=label);
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
     sample_path(trr::Tree,nPath=1, flag_show=true)
 
