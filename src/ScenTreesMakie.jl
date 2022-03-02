764bcca3322f200740146b2784beab64caba2da1
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-03-02 23:13:41 +0100

    asd

--- src/ScenTreesMakie.jl
+++ src/ScenTreesMakie.jl
@@ -15,6 +15,8 @@ include("StochPaths.jl")
 
 include("KernelDensityEstimation.jl")
 include("plots.jl")
+include("helper.jl")
+include("Wasserstein")
 
 
 export tree_approximation!,lattice_approximation,
