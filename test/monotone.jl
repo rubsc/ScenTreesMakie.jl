3723764b90629c6e85c0a3bb255b10d9dad9d12d
rubsc <58817466+rubsc@users.noreply.github.com>
2022-03-08 11:05:56 +0100

    Add files via upload

---
+++ test/monotone.jl
@@ -0,0 +1,14 @@
+# Make a given Tree monotone increasing 
+
+# First idea just bring children nodes to parent node level or above
+
+#for i in parent
+#    for j in children of i
+#        diff = state[j] - state[i]
+#        if diff < 0
+#            state[j] = state[i]
+#        end
+#    end
+#end
+
+
